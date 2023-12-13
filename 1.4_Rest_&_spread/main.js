7dbacf1e70e653df8242d0b2975ee8743e19e998
Hugo Rodríguez <hugorodriguezde@gmail.com>
2023-12-13 12:35:06 +0100

    added full excercises 1.4

--- 1.4_Rest_&_spread/main.js
+++ 1.4_Rest_&_spread/main.js
@@ -46,3 +46,43 @@ console.log(objecte2)
 
 //Ex 4
 
+const listaNumeros = [1, 2, 3, 4, 5, 6, 7];
+
+const [num1, num2, ...num3] = listaNumeros;
+
+console.log(num3)
+console.log(num1)
+console.log(num2)
+
+
+//Nivell 3
+
+//Ex 5
+
+
+const threeArg = ['pizza', 'hamburguer', 'pasta'];
+
+function threeArguments (arg1, arg2, arg3) {
+        console.log(arg1)
+        console.log(arg2)
+        console.log(arg3)
+}
+
+threeArguments(...threeArg)
+
+
+//Ex 6
+
+const obj1 = {
+    name: 'Pepito',
+    surname: 'Perez'
+}
+
+const obj2 = {
+    mail: 'pepitoperez@invento.com',
+    city: 'Barcelona'
+}
+
+const fullobj = {...obj1, ...obj2}
+
+console.log(fullobj)
